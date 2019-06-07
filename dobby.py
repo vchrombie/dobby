5b2f13b06ad473c0b1aff07a07c6a8166ec1e3c9
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2019-06-07 14:54:24 +0530

    added the st3 plugin code

---
+++ dobby.py
@@ -0,0 +1,22 @@
+import sublime
+import sublime_plugin
+
+import re
+
+from dobby.emojis import EMOJIS
+
+class DobbyCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # self.view.insert(edit, 0, "Hello, World!")
+        contents = self.view.substr(sublime.Region(0, self.view.size()))
+        region = sublime.Region(0, self.view.size())
+        self.view.replace(edit, region, self.emojify(contents))
+
+    def emojify(self, contents):
+        return self.emoji_replace(EMOJIS, contents)
+
+    def emoji_replace(self, edict, text):
+        # create a regular expression from all of the dictionary keys
+        regex = re.compile("|".join(map(re.escape, edict.keys(  ))))
+        # for each match, look up the corresponding value in the dictionary
+        return regex.sub(lambda match: edict[match.group(0)], text)
